ffaa0d9c591b4e12ef04d254103cf6f90a3eca84
Tyler Carrol <tylercarrol@hotmail.com>
2024-06-20 21:44:40 -0400

    Decorator Base Class & Decoratable

---
+++ TJC.Decorator/Decoratable.cs
@@ -0,0 +1,12 @@
+﻿namespace TJC.Decorator;
+
+public class Decoratable : IDecoratable
+{
+    public T? GetAs<T>()
+        where T : class =>
+        typeof(T) == typeof(Decoratable) ? this as T : null;
+
+    public bool IsType<T>()
+        where T : class =>
+        GetAs<T>() != null;
+}
---
+++ TJC.Decorator/DecoratorBaseClass.cs
@@ -0,0 +1,19 @@
+﻿namespace TJC.Decorator;
+
+public class DecoratorBaseClass<TDecoratable>(TDecoratable decoratable)
+    : IDecoratable
+    where TDecoratable : IDecoratable
+{
+    protected readonly TDecoratable Decoratable = decoratable;
+
+    public virtual T1? GetAs<T1>()
+        where T1 : class =>
+        GetType() == typeof(T1)
+     || GetType().GetInterfaces().Any(x => x == typeof(T1)) ?
+            this as T1 :
+            Decoratable.GetAs<T1>();
+
+    public bool IsType<T2>()
+        where T2 : class =>
+        GetAs<T2>() != null;
+}
